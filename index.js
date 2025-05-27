f81b4c8b65018321d4eca3d6868acb5826882f58
Liangwei <liangwei@live.com>
2025-05-28 00:28:14 +0800

    Expose Swagger UI in all environments

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 const port = 3000;
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-}
+// Swagger UI exposed in all environments (even production)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 
 
