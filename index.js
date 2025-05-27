06f06f122aa1d928778b46d6018a1e8f9954022b
Liangwei <liangwei@live.com>
2025-05-27 23:58:06 +0800

    Initial commit: setup basic API with Swagger

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const app = express();
+const port = 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
